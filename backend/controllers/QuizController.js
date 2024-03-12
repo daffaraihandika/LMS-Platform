59fffaacc48b08233d5f3ee32f1d915ef7cf1ea0
helsaalika <helsa.alika.tif421@polban.ac.id>
2024-03-12 13:49:15 +0700

    Backend Get All Quiz dan Get My Quiz

--- backend/controllers/QuizController.js
+++ backend/controllers/QuizController.js
@@ -1,5 +1,36 @@
-import { parseISO, endOfDay, startOfDay } from 'date-fns';
-import { PrismaClient } from '@prisma/client'
+import { parseISO, endOfDay, startOfDay } from "date-fns";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+export const getQuiz = async (req, res) => {
+  try {
+    const response = await prisma.Quiz.findMany({
+      include: {
+        user: true,
+        tags: true,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const getQuizByUser = async (req, res) => {
+  const userId = req.params.userId; // mengambil userId dari parameter
+  try {
+    const response = await prisma.Quiz.findMany({
+      where: {
+        userId: Number(userId), // filter berdasarkan userId
+      },
+      include: {
+        tags: true,
+        user: true,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { getQuiz, getQuizByUser } from "../controllers/QuizController.js";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('tess')
+router.get("/", (req, res) => {
+  res.send("tess");
 });
+
+router.get("/quizzes", getQuiz);
+router.get("/quizzes/user/:userId", getQuizByUser);
 
 export default router;
