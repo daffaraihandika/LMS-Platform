93a05db61355aaa25431a0daf3ed5fb78381dee1
daffaraihandika <draihandika@gmail.com>
2024-03-19 21:16:40 +0700

    create API tambah quiz

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { upload } from "../middleware/multer.js";
+
+import { createQuiz } from "../controllers/quizController.js";
+
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('tess')
 });
+
+router.post('/new-quiz', upload.single('image'), createQuiz)
 
 export default router;
